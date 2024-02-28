fdbb49bcc41a9b3f1e961ba07434f1aeb06d6127
jorgluiz <jorgeluizti@hotmail.com>
2024-02-28 05:38:18 -0300

    chore: 0.2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const mailer = require('./modules/mailer')
+const mailer = require('./modules/mailer');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
@@ -10,19 +10,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/stripe-notifications', (req, res) => {
   const paymentIntent = req.body.paymentIntent;
 
-    mailer.sendMail({ // https://mailtrap.io/
-      to: 'jorge@hotmail.com',
-      from: 'novo@hotmail.com',
-      template: 'auth/forgot_password',
-      context: { paymentIntent }
-    }, (err) => {
-      if (err) return res.status(400).send({ error: 'error' })
-    })
-
+  mailer.sendMail({
+    to: 'jorge@hotmail.com',
+    from: 'novo@hotmail.com',
+    template: 'auth/forgot_password',
+    context: { paymentIntent }
+  }, (err) => {
+    if (err) return res.status(400).send({ error: 'error' });
+    res.status(200).send('E-mail enviado com sucesso');
+  });
 });
 
+// Não é mais necessário definir a porta manualmente
+// O Vercel atribuirá automaticamente uma porta para o seu aplicativo
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+// Remova a definição da porta
+// const PORT = 3000;
+
+// Não é mais necessário usar app.listen()
+// O Vercel lidará com isso automaticamente
+
+// Remova app.listen()
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`);
+// });
